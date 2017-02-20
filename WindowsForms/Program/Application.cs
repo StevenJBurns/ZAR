5a4f3223ecb47b079af93c65a045c807aed5e4a9
SJB <stevenjburns@msn.com>
2017-02-19 17:24:49 -0700

    Import of stray source files from OneDrive

---
+++ WindowsForms/Program/Application.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace ZAR.WinForms
+	{
+	public class SingleInstanceApp : WindowsFormsApplicationBase
+		{
+		public SingleInstanceApp()
+            {
+            this.IsSingleInstance = true;
+            }
+
+        protected override void OnCreateMainForm()
+            {
+            this.MainForm = new FormMain();
+            }
+
+        [STAThread]
+        static void Main(string[] args)
+            {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            SingleInstanceApp app = new SingleInstanceApp();
+            app.Run(args);
+            }
+		}
+	}
+
